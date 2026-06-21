ddce5c6f2a071f27e12b0abf1d8ac33b26f21505
Murodjon Yuldashev <murodjonyuldasheev@gmail.com>
2026-06-21 11:33:56 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var adminClient *websocket.Conn
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Websocket ulanishda xato:", err)
+		return
+	}
+	defer conn.Close()
+
+	role := r.URL.Query().Get("role")
+
+	if role == "admin" {
+		adminClient = conn
+		log.Println("Developper Core (Admin) ulandi!")
+		for {
+			if _, _, err := conn.ReadMessage(); err != nil {
+				log.Println("Admin uzildi.")
+				adminClient = nil
+				break
+			}
+		}
+	} else {
+		log.Println("Sinfdosh (Foydalanuvchi) ulandi!")
+		for {
+			// Kadrlarni matn (Base64 string) ko'rinishida o'qiymiz
+			messageType, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("Sinfdosh uzildi.")
+				break
+			}
+
+			// Agar admin oynasi ochiq bo'lsa, srazu unga otamiz
+			if adminClient != nil {
+				err = adminClient.WriteMessage(messageType, message)
+				if err != nil {
+					log.Println("Adminga kadr yuborishda xato:", err)
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	// Statik HTML va video fayllarni server orqali tarqatish
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	http.HandleFunc("/ws", handleWebSocket)
+
+	// Render.com yoki boshqa internet xostinglar uchun portni dinamik olish
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Agar kompyuterda (lokalda) ishga tushsa, 8080 portni oladi
+	}
+
+	fmt.Println("Server ishga tushdi, port:", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("Serverni yuklashda xatolik:", err)
+	}
+}
